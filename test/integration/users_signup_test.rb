109d9f33f522991925712d264d4b8fa4a7bd9467
naoktym <naohiro.kata@gmail.com>
2019-10-28 01:14:03 +0900

    10.1.3 Testing unsuccessful edits

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -13,7 +13,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
       } }
     end
     assert_template 'users/new'
-    assert_select 'form[action=\'/signup\']'
+    assert_select 'form[action=\'/users\']'
     assert_select 'div#error_explanation'
     assert_select 'div.field_with_errors'
     assert_select 'div#error_explanation ul li', text: 'Name can\'t be blank'
